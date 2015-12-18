b9560bf00899ac3c10eae33427336005bbcbc77c
Adam Dahan <adamdahan@Adams-MacBook-Pro.local>
2015-12-18 15:36:01 -0500

    table view cell

---
+++ Sources/MaterialTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MaterialTableViewCell.swift
+//  MaterialKit
+//
+//  Created by Adam Dahan on 2015-12-18.
+//  Copyright © 2015 CosmicMind, Inc. All rights reserved.
+//
+
+import Foundation
