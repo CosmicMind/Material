3ff6b6f5e2d3ec0f3ef7061437dd2a83686ce1a0
danieldahan <daniel@graphkit.io>
2015-12-24 16:02:20 -0600

    updated LICENSE header

--- Sources/MaterialTableViewCell.swift
+++ Sources/MaterialTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2015 CosmicMind, Inc. <http://cosmicmind.io>
+// Copyright (C) 2015 CosmicMind, Inc. <http://cosmicmind.io>..
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
