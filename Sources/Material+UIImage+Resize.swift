ae58a867d96ffdd5fdaecfc721de14cfc511480d
Nam Nguyen - ナム <nam@ring.md>
2016-02-29 00:24:22 +0800

    Avoid blurry result in resizing and cropping image

--- Sources/Material+UIImage+Resize.swift
+++ Sources/Material+UIImage+Resize.swift
@@ -43,7 +43,7 @@ public extension UIImage {
 		
 		let g: UIImage?
 		let t: CGRect = CGRectMake(0, 0, w, h)
-		UIGraphicsBeginImageContext(t.size)
+		UIGraphicsBeginImageContextWithOptions(t.size, false, UIScreen.mainScreen().scale)
 		drawInRect(t, blendMode: .Normal, alpha: 1)
 		g = UIGraphicsGetImageFromCurrentImageContext()
 		UIGraphicsEndImageContext()
