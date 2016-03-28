55fcfcafae47202d31ba2661743cc90022a0625f
Jack Perry <jack.perry@me.com>
2016-03-27 21:44:31 -0400

    Added (required) UIKit import for MaterialCollectionViewDataSource and MaterialCollectionViewDelegate

--- Sources/MaterialCollectionViewDelegate.swift
+++ Sources/MaterialCollectionViewDelegate.swift
@@ -28,6 +28,8 @@
 * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+import UIKit
+
 public protocol MaterialCollectionViewDelegate : MaterialDelegate, UICollectionViewDelegate {
 	
 }
