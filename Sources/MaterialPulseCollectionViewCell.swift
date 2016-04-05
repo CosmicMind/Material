640bd928026a6ef04daf623e1a0693ab9462ec6e
Daniel Dahan <daniel@graphkit.io>
2016-04-05 11:32:54 -0400

    added MaterialColor to OSX

--- Sources/MaterialPulseCollectionViewCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MaterialPulseCollectionViewCell.swift
-//  Material
-//
-//  Created by Daniel Dahan on 2016-02-24.
-//  Copyright © 2016 CosmicMind, Inc. All rights reserved.
-//
-
-import Foundation
