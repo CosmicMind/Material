12d67e562e00b7dc2fd8f6b472a5f9886494c3db
Daniel Dahan <daniel@graphkit.io>
2016-04-04 23:13:21 -0400

    updated license format

--- Examples/Programmatic/SearchBarController/SearchBarController/AppSearchBarController.swift
+++ Examples/Programmatic/SearchBarController/SearchBarController/AppSearchBarController.swift
@@ -1,6 +1,5 @@
 /*
-* Copyright (C) 2015 - 2016, Daniel Dahan and CosmicMind, Inc. <http://cosmicmind.io>.
-* All rights reserved.
+* Copyright (C) 2015 - 2016, Daniel Dahan and CosmicMind, Inc. <http://cosmicmind.io>. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
