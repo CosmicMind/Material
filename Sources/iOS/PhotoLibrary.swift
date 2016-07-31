658026d5e7d40da05716ae80dcb51f1c95567b3c
Daniel Dahan <daniel@cosmicmind.io>
2016-07-31 16:15:01 -0400

    updated initial PhotoLibrary codebase to include a caching instance as well as collection dictionary

--- Sources/iOS/PhotoLibrary.swift
+++ Sources/iOS/PhotoLibrary.swift
@@ -78,16 +78,40 @@ public protocol PhotoLibraryDelegate {
     optional func photoLibrary(photoLibrary: PhotoLibrary, didChange changeInfo: PHChange)
     
     /**
-     Get changes objects in album.
+     A delegation method that is executed when changes are detected.
      - Parameter photoLibrary: A reference to the PhotoLibrary.
-     - Parameter object: A PHObject instance.
+     - Parameter beforeChanges: A PHObject before changes.
+     - Parameter afterChanges: A PHObject after changes.
+     - Parameter assetContentChanged: A Bool that is true if the image or video content for this
+     object has changed.
+     - Parameter objectWasDeleted: A Bool that is true if the object was deleted.
      */
     @objc
-    optional func photoLibrary(photoLibrary: PhotoLibrary, afterChanges object: PHObject)
+    optional func photoLibrary(photoLibrary: PhotoLibrary, beforeChanges: PHObject, afterChanges: PHObject, assetContentChanged: Bool, objectWasDeleted: Bool)
 }
 
 @objc(PhotoLibrary)
 public class PhotoLibrary: NSObject {
+    /// A reference to the PHCachingImageManager.
+    public private(set) lazy var cachingImageManager = PHCachingImageManager()
+    
+    /// A reference to the currently fetched album.
+    public private(set) var fetchResult: PHFetchResult<PHAssetCollection>?
+    
+    /// The assets used in the album.
+    public private(set) var collections: [PHAssetCollection: [PHAsset]]! {
+        willSet {
+            cachingImageManager.stopCachingImagesForAllAssets()
+        }
+        
+        didSet {
+            cachingImageManager.allowsCachingHighQualityImages = true
+            for (_, assets) in collections {
+                cachingImageManager.startCachingImages(for: assets, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: nil)
+            }
+        }
+    }
+    
     /// A reference to a PhotoLibraryDelegate.
     public weak var delegate: PhotoLibraryDelegate?
     
@@ -105,16 +129,6 @@ public class PhotoLibrary: NSObject {
     public override init() {
         super.init()
         prepare()
-    }
-    
-    /// A method used to prepare the instance object.
-    public func prepare() {
-        prepareChangeObservers()
-    }
-    
-    /// A method used to enable change observation.
-    public func prepareChangeObservers() {
-        PHPhotoLibrary.shared().register(self)
     }
     
     /**
@@ -154,29 +168,83 @@ public class PhotoLibrary: NSObject {
         }
     }
     
-    public func moments(in momentList: PHCollectionList, options: PHFetchOptions?) -> [PHAssetCollection] {
-        var v = [PHAssetCollection]()
-        PHAssetCollection.fetchMoments(inMomentList: momentList, options: options).enumerateObjects(options: EnumerationOptions.concurrent) { (collection, _, _) in
-            v.append(collection)
+    /**
+     Fetch different PHAssetCollections asynchronously based on different types and subtypes
+     with an optional completion block.
+     - Parameter type: A PHAssetCollectionType.
+     - Parameter subtype: A PHAssetCollectionSubtype.
+     - Parameter completion: An optional completion block.
+     */
+    public func fetch(type: PHAssetCollectionType, subtype: PHAssetCollectionSubtype, completion: ([PHAssetCollection: [PHAsset]]) -> Void) {
+        DispatchQueue.global(attributes: DispatchQueue.GlobalAttributes.qosDefault).async { [weak self, type = type, subtype = subtype, completion = completion] in
+            guard let s = self else {
+                return
+            }
+            
+            defer {
+                DispatchQueue.main.async { [weak self] in
+                    completion(s.collections)
+                }
+            }
+            
+            let options = PHFetchOptions()
+            options.includeHiddenAssets = true
+            options.includeAllBurstAssets = true
+            options.wantsIncrementalChangeDetails = false
+            
+            s.fetchResult = PHAssetCollection.fetchAssetCollections(with: type, subtype: subtype, options: options)
+            s.fetchResult?.enumerateObjects(options: []) { [weak self] (collection, _, _) in
+                guard let s = self else {
+                    return
+                }
+                
+                let options = PHFetchOptions()
+                let descriptor = SortDescriptor(key: "creationDate", ascending: false)
+                options.sortDescriptors = [descriptor]
+                options.includeHiddenAssets = true
+                options.includeAllBurstAssets = true
+                
+                var assets = [PHAsset]()
+                PHAsset.fetchAssets(in: collection, options: options).enumerateObjects(options: []) { (asset, _, _) in
+                    assets.append(asset)
+                }
+                
+                s.collections[collection] = assets
+            }
         }
-        return v
     }
     
-    public func fetch(with type: PHAssetCollectionType, subtype: PHAssetCollectionSubtype, options: PHFetchOptions?) -> PHFetchResult<PHAssetCollection> {
-        let result = PHAssetCollection.fetchAssetCollections(with: type, subtype: subtype, options: options)
-        return result
+    /// A method used to prepare the instance object.
+    private func prepare() {
+        prepareCollections()
+        prepareChangeObservers()
     }
     
+    /// Prepares the collections.
+    private func prepareCollections() {
+        collections = [PHAssetCollection: [PHAsset]]()
+    }
+    
+    /// A method used to enable change observation.
+    private func prepareChangeObservers() {
+        PHPhotoLibrary.shared().register(self)
+    }
+    
+//    public func moments(in momentList: PHCollectionList, options: PHFetchOptions?) -> [PHAssetCollection] {
+//        var v = [PHAssetCollection]()
+//        PHAssetCollection.fetchMoments(inMomentList: momentList, options: options).enumerateObjects(options: EnumerationOptions.concurrent) { (collection, _, _) in
+//            v.append(collection)
+//        }
+//        return v
+//    }
+//    
+//    public func fetch(with type: PHAssetCollectionType, subtype: PHAssetCollectionSubtype, options: PHFetchOptions?) -> PHFetchResult<PHAssetCollection> {
+//        let result = PHAssetCollection.fetchAssetCollections(with: type, subtype: subtype, options: options)
+//        return result
+//    }
+//    
     public func performChanges(_ changeBlock: () -> Void, completionHandler: ((Bool, NSError?) -> Void)? = nil) {
         PHPhotoLibrary.shared().performChanges(changeBlock, completionHandler: completionHandler)
-    }
-    
-    public func assets(in album: PHAssetCollection, options: PHFetchOptions?) -> [PHAsset] {
-        var v = [PHAsset]()
-        PHAsset.fetchAssets(in: album, options: options).enumerateObjects(options: []) { (asset, _, _) in
-            v.append(asset)
-        }
-        return v
     }
     
     public func image(for asset: PHAsset, targetSize: CGSize, contentMode: PHImageContentMode, options: PHImageRequestOptions?, completion: (UIImage?, [NSObject: AnyObject]?) -> Void) -> PHImageRequestID {
@@ -186,21 +254,9 @@ public class PhotoLibrary: NSObject {
     public func data(for asset: PHAsset, options: PHImageRequestOptions?, completion: (Data?, String?, UIImageOrientation, [NSObject: AnyObject]?) -> Void) -> PHImageRequestID {
         return PHImageManager.default().requestImageData(for: asset, options: options, resultHandler: completion)
     }
-    
+
     public func cancel(for requestID: PHImageRequestID) {
         PHImageManager.default().cancelImageRequest(requestID)
-    }
-}
-
-/// Albums.
-extension PhotoLibrary {
-    /**
-     Creates an album with a given title.
-     - Parameter album title: A String.
-     - Returns: A PHObjectPlaceholder.
-     */
-    public func create(album title: String) -> PHObjectPlaceholder {
-        return PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: title).placeholderForCreatedAssetCollection
     }
 }
 
@@ -211,7 +267,27 @@ extension PhotoLibrary: PHPhotoLibraryChangeObserver {
      - Parameter _ changeInstance: A PHChange obejct describing the changes in the
      photo library.
      */
-    public func photoLibraryDidChange(_ changeInstance: PHChange) {
-        delegate?.photoLibrary?(photoLibrary: self, didChange: changeInstance)
+    public func photoLibraryDidChange(_ changeInfo: PHChange) {
+        delegate?.photoLibrary?(photoLibrary: self, didChange: changeInfo)
+        
+        guard let result = fetchResult else {
+            return
+        }
+        
+        result.enumerateObjects(options: []) { [weak self, changeInfo = changeInfo] (collection, _, _) in
+            guard let s = self else {
+                return
+            }
+            
+            guard let details = changeInfo.changeDetails(for: collection) else {
+                return
+            }
+            
+            guard let afterChanges = details.objectAfterChanges else {
+                return
+            }
+            
+            s.delegate?.photoLibrary?(photoLibrary: s, beforeChanges: details.objectBeforeChanges, afterChanges: afterChanges, assetContentChanged: details.assetContentChanged, objectWasDeleted: details.objectWasDeleted)
+        }
     }
 }
