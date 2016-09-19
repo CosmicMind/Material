18e10652b1e333da0e6726c123bd790ffe4181ed
Daniel Dahan <daniel@cosmicmind.io>
2016-09-19 14:28:30 -0400

    development: updated PhotoLibrary sample

--- Sources/iOS/CollectionReusableView.swift
+++ Sources/iOS/CollectionReusableView.swift
@@ -30,8 +30,8 @@
 
 import UIKit
 
-@objc(MaterialCollectionReusableView)
-open class MaterialCollectionReusableView: UICollectionReusableView {
+@objc(CollectionReusableView)
+open class CollectionReusableView: UICollectionReusableView {
 	/**
      A CAShapeLayer used to manage elements that would be affected by
      the clipToBounds property of the backing layer. For example, this
