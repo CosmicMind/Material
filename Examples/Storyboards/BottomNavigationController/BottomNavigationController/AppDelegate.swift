7fa5c39327fb180ae74ac9d8d1d2b33ec749a849
adamdahan <adam@88labs.com>
2016-09-30 15:37:20 -0400

    Fixing bottom nav

--- Examples/Storyboards/BottomNavigationController/BottomNavigationController/AppDelegate.swift
+++ Examples/Storyboards/BottomNavigationController/BottomNavigationController/AppDelegate.swift
@@ -19,31 +19,5 @@ extension UIStoryboard {
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    lazy var videoViewController: VideoViewController = {
-        return UIStoryboard.viewController(identifier: "VideoViewController") as! VideoViewController
-    }()
-    
-    lazy var audioViewController: AudioViewController = {
-        return UIStoryboard.viewController(identifier: "AudioViewController") as! AudioViewController
-    }()
-    
-    lazy var photoViewController: PhotoViewController = {
-        return UIStoryboard.viewController(identifier: "PhotoViewController") as! PhotoViewController
-    }()
-    
-    lazy var remindersViewController: RemindersViewController = {
-        return UIStoryboard.viewController(identifier: "RemindersViewController") as! RemindersViewController
-    }()
-    
-    lazy var searchViewController: SearchViewController = {
-        return UIStoryboard.viewController(identifier: "SearchViewController") as! SearchViewController
-    }()
-
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window!.rootViewController = AppBottomNavigationController(viewControllers: [PhotoViewController(), VideoViewController(), AudioViewController(), RemindersViewController(), SearchViewController()])
-        window!.makeKeyAndVisible()
-    }
 }
 
