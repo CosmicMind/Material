d41c5c192a9be640688ead437f2b96e6734f7b5c
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2018-06-28 21:08:28 +0400

    Added missing iPhoneSE identifier

--- Sources/iOS/Device.swift
+++ Sources/iOS/Device.swift
@@ -95,7 +95,7 @@ public struct Device {
     case "iPhone7,1":									return .iPhone6Plus
     case "iPhone8,1":									return .iPhone6s
     case "iPhone8,2":									return .iPhone6sPlus
-    case "iPhone8,4":									return .iPhoneSE
+    case "iPhone8,3", "iPhone8,4":									return .iPhoneSE
     case "iPhone9,1", "iPhone9,3":                      return .iPhone7
     case "iPhone9,2", "iPhone9,4":                      return .iPhone7Plus
     case "iPhone10,1", "iPhone10,4":                    return .iPhone8
